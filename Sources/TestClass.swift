445287dbdc2679c092ec91892169aff8056ed8a8
Sandeep Suthar <sandeep.suthar@qburst.com>
2024-01-02 11:39:12 +0530

    inital cimmit

---
+++ Sources/TestClass.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sandeep on 02/01/24.
+//
+
+import Foundation
+
+public class TestClass {
+    public init() { }
+    public func printHello() {
+        print("Hello world")
+    }
+}

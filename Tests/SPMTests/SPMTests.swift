007353d389cd745ae3f3cd29e23e060782e17ec0
Sandeep Suthar <sandeep.suthar@qburst.com>
2024-01-03 11:32:01 +0530

    Unit test case added for Fetch Data

--- Tests/SPMTests/SPMTests.swift
+++ Tests/SPMTests/SPMTests.swift
@@ -3,7 +3,17 @@ import XCTest
 
 final class SPMTests: XCTestCase {
     func testExample() throws {
+        let expectation = self.expectation(description: "Completion should be called")
         let test = TestClass()
-        test.fetchData()
+        test.fetchData { result in
+            switch result {
+                case .success(let todo):
+                    XCTAssertNotNil(todo.title)
+                case .failure(let error):
+                    XCTFail("Unexpected failure: \(error)")
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
     }
 }

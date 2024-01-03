b61cd60032311cbe0b708d90ccf50930fef23f38
Sandeep Suthar <sandeep.suthar@qburst.com>
2024-01-03 11:02:26 +0530

    dependency added

--- Package.swift
+++ Package.swift
@@ -9,15 +9,21 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SPM",
-            targets: ["SPM"]),
+            targets: ["SPM"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.8.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SPM"),
+            name: "SPM", dependencies: ["Alamofire"]
+        ),
         .testTarget(
             name: "SPMTests",
-            dependencies: ["SPM"]),
+            dependencies: ["SPM"]
+        ),
     ]
 )
--- Sources/SPM/TestClass.swift
+++ Sources/SPM/TestClass.swift
@@ -6,10 +6,28 @@
 //
 
 import Foundation
+import Alamofire
 
 public class TestClass {
     public init() { }
     public func printHello() {
         print("Hello world")
+    }
+
+    public func fetchData() {
+        AF.request("https://jsonplaceholder.typicode.com/todos/1", method: .get)
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .responseData { response in
+                switch response.result {
+                    case .success(let data):
+                        print("Validation Successful")
+                        if let result = try? JSONSerialization.jsonObject(with: data) {
+                            print(result)
+                        }
+                    case let .failure(error):
+                        print(error)
+                }
+            }
     }
 }

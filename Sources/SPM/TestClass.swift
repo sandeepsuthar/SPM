f66d1cc2f40aa906421ca4516917683b28232d10
Sandeep Suthar <sandeep.suthar@qburst.com>
2024-01-03 11:22:39 +0530

    response decoded

--- Sources/SPM/TestClass.swift
+++ Sources/SPM/TestClass.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Sandeep on 02/01/24.
 //
@@ -8,26 +8,32 @@
 import Foundation
 import Alamofire
 
+public struct Todo: Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let completed: Bool
+}
+
 public class TestClass {
     public init() { }
     public func printHello() {
         print("Hello world")
     }
 
-    public func fetchData() {
+    public func fetchData(completion: @escaping (Result<Todo, AFError>) -> Void) {
         AF.request("https://jsonplaceholder.typicode.com/todos/1", method: .get)
             .validate(statusCode: 200..<300)
             .validate(contentType: ["application/json"])
             .responseData { response in
-                switch response.result {
-                    case .success(let data):
-                        print("Validation Successful")
-                        if let result = try? JSONSerialization.jsonObject(with: data) {
-                            print(result)
-                        }
-                    case let .failure(error):
-                        print(error)
-                }
+                completion(response.result.flatMap { data in
+                    do {
+                        let decodedData = try JSONDecoder().decode(Todo.self, from: data)
+                        return .success(decodedData)
+                    } catch {
+                        return .failure(AFError.responseSerializationFailed(reason: .inputDataNilOrZeroLength))
+                    }
+                })
             }
     }
 }
